8fd3eccda4dbbaf4cfaab96de713abcebb0990da
mykhailoivaniuk <65302583+mykhailoivaniuk@users.noreply.github.com>
2022-01-24 08:49:10 -0600

    Time: 0 ms (100.00%), Space: 40 MB (20.61%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -12,16 +12,17 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode cur = head;
         ListNode prev = null;
+        return recursiveHelper(cur,prev);
         
-        while (cur != null){
-            ListNode next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-            
+    }
+    public ListNode recursiveHelper(ListNode cur, ListNode prev){
+        if (cur == null){
+            return prev;
         }
-        
-        return prev;
+        ListNode next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        return recursiveHelper(next, prev);
         
     }
 }
