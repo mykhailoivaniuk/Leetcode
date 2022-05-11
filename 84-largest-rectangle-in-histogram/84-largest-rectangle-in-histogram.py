e50a2df9449402810f6875d1113e3323da7ff0cb
mykhailoivaniuk <65302583+mykhailoivaniuk@users.noreply.github.com>
2022-05-11 01:12:19 -0500

    Time: 1107 ms (73.58%), Space: 28.5 MB (49.90%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,25 +1,14 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        size = len(heights)
-        next_smaller = [size - i for i in range(size)]
-        prev_smaller = [i+1 for i in range(size)]
-        mono_stack = deque()
+        inc_stack = deque()
+        inc_stack.append(-1)
+        heights.append(0)
+        max_area = 0
         for i, height in enumerate(heights):
-            while len(mono_stack) != 0 and heights[mono_stack[-1]] >= height:
-                idx = mono_stack.pop()
-                next_smaller[idx] = i - idx
-            mono_stack.append(i)
-        mono_stack.clear()
-        for i in range(size - 1, -1, -1):
-            while len(mono_stack) != 0 and heights[mono_stack[-1]] > heights[i]:
-                idx = mono_stack.pop()
-                prev_smaller[idx] = idx - i
-            mono_stack.append(i)
-        cur_max = 0
-        for i, dist in enumerate(next_smaller):
-                cur_max = max(cur_max, heights[i]*(next_smaller[i]+prev_smaller[i]-1))
-        return cur_max
+            while heights[inc_stack[-1]] > height:
+                h = heights[inc_stack.pop()]
+                w = i - inc_stack[-1] - 1
+                max_area = max(max_area, w*h)
+            inc_stack.append(i)
+        return max_area
             
-            
-            
-        
