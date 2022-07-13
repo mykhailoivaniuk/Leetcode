593590141605710fa9e5dd2af07bc51c3a900143
mykhailoivaniuk <65302583+mykhailoivaniuk@users.noreply.github.com>
2022-07-12 22:36:32 -0700

    Time: 2 ms (92.48%), Space: 66.9 MB (79.90%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,18 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
         if(nums.length == 1) return true;
-        int[] waysToReach = new int[nums.length];
-        waysToReach[0] = 1;
-        for(int i = 0; i<nums.length - 1; i++){
-            if(waysToReach[i] != 0){
-                for(int j = nums[i]; j > 0; j--){
-                    if(i + j < nums.length){
-                        waysToReach[i+j] += 1;
-                    }
-                }
-            }
+        int goal = nums.length - 1;
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(i + nums[i] >= goal) goal = i;
         }
-        return waysToReach[nums.length - 1] != 0;
+        
+        return goal == 0;
         
     }
 }
