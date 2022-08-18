df844416721bd8d9cca5c148ef261308f70ce0fc
mykhailoivaniuk <65302583+mykhailoivaniuk@users.noreply.github.com>
2022-08-17 20:45:30 -0700

    Time: 1 ms (95.16%), Space: 46.5 MB (9.58%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.java
+++ 766-toeplitz-matrix/766-toeplitz-matrix.java
@@ -1,25 +1,9 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        for(int i = matrix.length - 1; i > -1; i--){
-            if(!checkDiagonal(matrix, i, 0)){
-                return false;
+        for(int i = 0; i < matrix.length - 1; i++){
+            for(int j = 0; j < matrix[i].length - 1; j++){
+                if(matrix[i][j] != matrix[i+1][j+1]) return false;
             }
-        }
-        for(int j = 1; j<matrix[0].length; j++){
-            if(!checkDiagonal(matrix, 0, j)) return false;
-        }
-        return true;
-    }
-    
-    public boolean checkDiagonal(int[][] matrix, int row, int col){
-        if(row >= matrix.length || col >= matrix[0].length){
-            return true;
-        }
-        int diagVal = matrix[row][col];
-        while(row < matrix.length && col< matrix[0].length){
-            if(matrix[row][col] != diagVal) return false;
-            row++;
-            col++;
         }
         return true;
     }
